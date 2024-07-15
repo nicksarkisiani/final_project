a8b6f5de55f68dd767b116d0ab8b237fce4c58ad
Haster1337 <hasterpro@gmail.com>
2024-07-16 02:29:20 +0400

    new

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,13 +1,43 @@
 const swiper = new Swiper('.image-slider', {
     loop: true,
     hashNavigation: true,
+    enabled: true,
     thumbs: {
         swiper: {
             el: '.image-mini-slider',
-            slidesPerView: 8,
-            spaceBetween: '12px'
+            slidesPerView: 3,
+            spaceBetween: '6px',
+            breakpoints: {
+                950: {
+                    slidesPerView: 8,
+                    spaceBetween: '12px',
+                    width: null,
+                }
+            }
         }
     },
     centeredSlides: true,
+    navigation: {
+        nextEl: '',
+        prevEl: '',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 3,
+    },
+    breakpoints: {
+        950: {
+            pagination: {
+                el: ''
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        }
+    }
 });
+
 
